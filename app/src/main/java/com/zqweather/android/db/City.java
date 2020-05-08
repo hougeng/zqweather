8220ab634bfa866c72292f9bd293dfdf1ede4ed9
hougeng <liyehuizuishuai@163.com>
2020-05-08 17:31:52 +0800

    创建数据库和表

---
+++ app/src/main/java/com/zqweather/android/db/City.java
@@ -0,0 +1,42 @@
+package com.zqweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceCode;  //当前市归属省的id值
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
